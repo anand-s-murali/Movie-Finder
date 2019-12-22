1d320ae5b0670899cebe9af2b7b9ff008fb0ea64
asmural <58267405+anand-s-murali@users.noreply.github.com>
2019-12-22 10:44:55 -0500

    Scrapy commit
    Made movies.py use Scrapy to parse the webpage. This is because beautifulsoup and requests will sometimes fails. This is most likely caused by the requested site being a javascript page, and hence, the page may or may not load correctly. Using Scrapy and Splash, we can overcome this.

--- movies.py
+++ movies.py
@@ -1,99 +1,104 @@
-from bs4 import BeautifulSoup
-from google import google
-from selenium import *
-import requests
+import scrapy
+from scrapy_splash import SplashRequest
+from scrapy.utils.project import get_project_settings
+from scrapy.crawler import CrawlerProcess
 import sys
-import traceback
-import re
+import time
+from google import google
+#from bs4 import BeautifulSoup
+#import requests
 
+class MovieItem(scrapy.Item):
+    title = scrapy.Field()
+    synopsis = scrapy.Field()
+    cast = scrapy.Field()
+    rating = scrapy.Field()
 
-# Now we need a function to handle the actual web scraping
-# This method will make use of beautifulsoup, and should take in,
-# just for starters, the title of the movie we want
-def scrape(query):
-    # get the rotten tomatoes page using google search
-    
-    # store the first page of search results
-    num_page = 1
-    search_results = google.search(query, num_page) # the list of results
+class MovieSpider(scrapy.Spider):
+    # name the spider and provide start url
+    name = "movie_spider"
+    start_urls = []
+
+    # define constructor; just needs the url from google
+    def __init__(self, *args, **kwargs):
+        # some funky business
+        super(MovieSpider, self).__init__(*args, **kwargs)
         
+        # get the url parameter
+        url = kwargs.get("url")
+        # make sure url is found and is appropriate
+        if not url:
+            raise ValueError("Improper url given")
+        if not isinstance(url, str):
+            print(type(url))
+            raise ValueError("url passed is not a string!")
+    
+        # add the url to the start_url list
+        self.start_urls.append(url)
+
+    # perform the scraping over the given links (just one for now)
+    def start_requests(self):
+        for url in start_urls:
+            # get splash request
+            yield SplashRequest(url=url, callback=self.parse, endpoint='render.html')
+    
+    # performs the page parsing
+    def parse(self, response):
+        # TODO
+        print("HERE")
+        print(response.url)
+
+
+def scrape(query):
+    # get the rotten tomatoes page using google search and store the first page of search results
+    num_page = 1
+    search_results = google.search(query, num_page)
+    while(len(search_results) != 10):
+        # sleep for a little bit so not to get banned
+        print("sleeping...")
+        time.sleep(2)
+        search_results = google.search(query,num_page)
+
+
     try:
-        # get rotten tomato link 
+        # get rotten tomato link
         URL = search_results[0].link
         if URL == "https://www.rottentomatoes.com/":
             print("No movie specified!")
             sys.exit(1)
         elif "/search" in URL:
             print("Not allowed to crawl here!")
-            sys.exit(0)
-        response = requests.get(URL)
-        # make sure request was valid
-        if response.status_code != 200:
-            print("Error retrieving the url specified by {}".format(URL))
             sys.exit(1)
-    
-        soup = BeautifulSoup(response.content, features="lxml")
-
-        # now that we have the webpage, we need to get the information we need!
-        # we need to get cast information, movie rating, and plot overview, and link to trailer
         
-        # First get the title of the movie
-        movie_title = soup.find("title").text
-        print(movie_title)
-        
-        # now get synopsis
-        movie_synopsis = soup.find(id="movieSynopsis")
-        print("Synopsis:\n",movie_synopsis)
-
-        # now get cast
-        #movie_cast = soup.find_all("div", class_="cast-item media inlineBlock")
-        cast = []
-        for cast_item in soup.find_all("div", class_="cast-item media inlineBlock"):
-            actor_name = cast_item.find("span").text.strip() 
-            # now get role
-            role = cast_item.find("span", class_="characters subtle smaller").text.strip()
-            # remove new lines and possible carriage returns
-            
-            split_str = role.split()
-            role = ""
-            for w in split_str:
-                if w != "":
-                    role += (w+" ")
-            role = role.strip()
-            cast.append(actor_name+" "+role)
-        
-        # print the cast
-        print("Cast:")
-        for a in cast:
-            print(a)
+        # now that we have the link, we need to scrape the page
+        # this will/must be handled by crawler process!
+        process = CrawlerProcess(get_project_settings())
+        process.crawl(MovieSpider, url=URL)
+        # this call will block until cralwer is finished
+        #process.start()
+    except IndexError:
+        print(search_results)
+        sys.exit(1)
     except Exception as e:
         print(e)
-        print(traceback.format_exc())
         sys.exit(1)
 
-
+# main method
 def main():
     # main code goes here
-    '''
-    this is how we will handle the system arguments
-    print("Number of system arguments: {}".format(len(sys.argv)))
-
-    # print all the arguments
-    for a in sys.argv:
-        print("{} ".format(a), end="")
-    '''
     # make sure to pass title in as one word! This means you need to pass in the title
-    # in quotes when passing in as system argument!
+    # in quotes if the title is multiple words when passing in as system argument!
     try:
-        title = sys.argv[1] + "rotten tomatoes"
+        movie_query = sys.argv[1] + "rotten tomatoes"
     except Exception as e:
         print("No movie specified.")
-        sys.exit(1)
+        sys.exit(0)
     
-    scrape(title)
+    scrape(movie_query)
+
 
 # this will be the first thing to be run when the script opens
 # it is only responsible for calling the main() function above it
 # think about this as int main() in C or main in java
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
